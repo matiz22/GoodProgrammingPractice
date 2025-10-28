9d9fb7f79b8b26c384043f18afa7d62e95cb3c8f
matiz22 <mateuszmalich@gmail.com>
2025-10-28 17:24:33 +0100

    CU-86c63vnr6[complete] - routes

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ from typing import Union
 
 from fastapi import FastAPI
 
+from links import links_routes
+from movies import movies_routes
+from ratings import ratings_routes
+from tags import tags_routes
+
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+app.include_router(movies_routes.router)
+app.include_router(links_routes.router)
+app.include_router(ratings_routes.router)
+app.include_router(tags_routes.router)
